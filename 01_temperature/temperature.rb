b26e040b067330a7346b93f35d038a5b0896b7bd
kfarfan <kevinfarfan@gmail.com>
2018-10-02 21:27:50 -0400

    completed first two

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc (val)
+    (val - 32 ) * (5.0/9.0);
+end
+
+def ctof (val)
+    (val * (9.0/5.0)) + 32
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
 #write your code here
+
+def echo (string)
+    string
+end
+
+def shout (string)
+    string.upcase
+end
+
+def repeat(string, args1 = 2)
+    Array.new(args1, string).join(" ");
+    
+end
+
+def start_of_word (string, n = 0) 
+    string.slice(0,n)
+
+end
+
+def first_word (string) 
+    string.slice(0, string.index(' '))
+end
+
+def titleize(string)
+    list = string.split(" ")
+    list[0].capitalize!
+    list[1..-1].each do |word| 
+      word.capitalize! unless little_word?(word)
+    end
+    if list.length > 1
+      final_list = list.inject(""){ |line, word| line + " #{word}"}.strip
+    else
+      string.capitalize
+    end
+  end
+  
+  private
+  
+  def little_word?(string)
+    ["and", "or", "of", "in", "a", "the", "over"].include?(string)
+  end
